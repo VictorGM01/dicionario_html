608726d42abf4c934b1cf279cf49f0f1c7625c18
VictorGM01 <victormarques8801@gmail.com>
2021-08-17 17:47:18 -0300

    # Dicionario para manipulação das tags HTML

---
+++ src/dicionario/dicionario.py
@@ -0,0 +1,34 @@
+import io
+
+
+class DicionarioTags:
+    def __init__(self):
+        self.__arquivo = io.open("tags.txt", "r", encoding='utf8')
+
+    def mostra_todas_as_tags(self):
+        with self.__arquivo as arquivo:
+            for linha in arquivo:
+                print(linha)
+
+    def adiciona_tag(self, tag):
+        arquivo = io.open("tags.txt", "a", encoding="utf8")
+        funcao = str(input("Digite a função desta tag: "))
+        with arquivo:
+            arquivo.write("\n")
+            arquivo.write(f"tag: {tag} --->>> Função: {funcao}")
+            print(f"Tag: '{tag}' adicionada com sucesso!!")
+
+    def busca_tag(self, tag):
+        with self.__arquivo as arquivo:
+            for linha in arquivo:
+                if tag in linha.split(" --->>>")[0]:
+                    busca = linha
+                    print(busca)
+
+    def busca_funcao(self, palavra_chave):
+        palavra_chave = str(palavra_chave).lower()
+        with self.__arquivo as arquivo:
+            for linha in arquivo:
+                if palavra_chave in linha:
+                    busca_funcao = linha
+                    print(busca_funcao)
