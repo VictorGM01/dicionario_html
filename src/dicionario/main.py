13caaa4529b97517264a445882b85147d41b7096
VictorGM01 <victormarques8801@gmail.com>
2021-08-17 17:53:35 -0300

    # Arquivo que utiliza os métodos do arquivo "dicionario.py" para adicionar, buscar e mostrar as tags

---
+++ src/dicionario/main.py
@@ -0,0 +1,8 @@
+from src.dicionario.dicionario import DicionarioTags
+
+dicionario = DicionarioTags()
+
+dicionario.adiciona_tag("tag que será adicionada")
+dicionario.busca_tag("tag que está sendo procurada")
+dicionario.busca_funcao("função específica que está sendo procurada")
+dicionario.mostra_todas_as_tags()
